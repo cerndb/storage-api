ff0597b077cfa639498de52ee693d663ebbd8c5c
Ruben Domingo Gaspar Aparicio <rgaspar@storagedb.cern.ch>
2016-06-02 18:42:18 +0200

    new vendors

---
+++ storage/vendors/Cephops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+class Cephops(BasicStorage):
+    pass
