7ea4fe598920bbf135f1ae3d20e12aea0145eba3
Albin Stjerna <albin.stjerna@cern.ch>
2017-01-23 17:26:56 +0100

    Some more back-end tests

--- extensions/tests/test_storage.py
+++ extensions/tests/test_storage.py
@@ -55,6 +55,23 @@ def test_get_snapshots(storage):
 def test_add_policy(storage):
     volume_name = uuid.uuid1()
     storage.create_volume(name=volume_name)
+    rules = ["host1.db.cern.ch", "*db.cern.ch"]
 
-    storage.add_policy(volume_name, "a policy", ["host1.db.cern.ch",
-                                                 "*db.cern.ch"])
+    storage.add_policy(volume_name, "a policy", rules)
+
+    policies = storage.policies(volume_name)
+
+    assert len(policies) == 1
+    assert policies[0]['policy_name'] == "a policy"
+    assert policies[0]['rules'] == rules
+
+
+@pytest.mark.parametrize("storage", [DummyStorage()])
+def test_delete_policy(storage):
+    volume_name = uuid.uuid1()
+    storage.create_volume(name=volume_name)
+    rules = ["host1.db.cern.ch", "*db.cern.ch"]
+
+    storage.add_policy(volume_name, "a policy", rules)
+    storage.remove_policy(volume_name, "a policy")
+    assert len(storage.policies(volume_name)) == 0
