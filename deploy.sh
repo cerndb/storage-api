512569ec3ac5308aa37a877c2dbf7bf8ec528ff2
Albin Stjerna <albin.stjerna@gmail.com>
2017-06-19 14:41:50 +0200

    First attempt at porting everything to GitLab

--- deploy.sh
+++ deploy.sh
@@ -14,25 +14,19 @@ rm -rf $PAGES_REPO_DIR
 echo "Adding files from $TARGET:"
 ls -al $TARGET
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy"
-    exit 0
-fi
-
 # # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+#chmod 600 deploy_key
+#eval `ssh-agent -s`
+#ssh-add deploy_key
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+#SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 # COMMIT_AUTHOR_EMAIL=`git config user.email`
 
@@ -50,7 +44,7 @@ touch .nojekyll
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "$GITLAB_USER_EMAIL"
 
 rm -rf ./**/*
 
@@ -70,4 +64,4 @@ fi
 git commit -a -m "Deploy to GitHub Pages: ${SHA}"
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $CI_REPOSITORY_URL  $TARGET_BRANCH
