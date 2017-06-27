77c7d25c7988925be3f23059fdcecdf01a3c03dc
Albin Stjerna <albin.stjerna@gmail.com>
2017-06-27 15:44:18 +0200

    Make sure the aggregate is not None before trying to filter it

--- storage_api/apis/__init__.py
+++ storage_api/apis/__init__.py
@@ -17,7 +17,7 @@ authorizations = {}
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 api = Api(
     title='CERN Unified Storage API',
