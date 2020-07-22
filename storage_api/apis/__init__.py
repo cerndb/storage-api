5dffd88488f4ae5ef33c30f17addfc9b9f0c145a
Borja Aparicio <borja.aparicio.cotarelo@cern.ch>
2020-07-22 12:26:18 +0200

    IA-1699: Increase UWSGI timeout for workers

--- storage_api/apis/__init__.py
+++ storage_api/apis/__init__.py
@@ -17,7 +17,7 @@ from .common.auth import authorizations
 
 log = init_logger()
 
-__version__ = '3.3.5'
+__version__ = '3.3.6'
 __major_version__ = __version__.split(".")[0]
 INTROSPECTION_MOUNTPOINT = "/conf"
 SAPI_MOUNTPOINT = "/v{}".format(__major_version__)
